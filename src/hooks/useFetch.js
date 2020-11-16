b7bc8502d2081ed08a380b901ec7c325c565d60e
Marcos Gomez <marcos_go2015@hotmail.com>
2020-11-16 01:42:48 -0300

    Pruebas sobre useFetch y CustomHook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -29,6 +29,13 @@ export const useFetch = (url) => {
                     })   
                 }
         })
+        .catch(()=>{
+            setState({
+                data:null,
+                loading:false,
+                error: 'No se pudo cargar la info'
+            })
+        })
     }, [url])
 
     return state;
